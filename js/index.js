259bcec9ce50b399510fb1a0093d88833c5e3d22
Alex Barbero <barrufat@hotmail.com>
2023-09-27 13:42:29 +0200

    Add get-random-cards function

--- js/index.js
+++ js/index.js
@@ -1,20 +1,35 @@
 const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-const figures = ["♠", "♣", "♦", "♥"];
+const suits = ["♠", "♣", "♦", "♥"];
+
+const deck = [];
+
 const startButtonElement = document.querySelector(".start-game");
 const firstScreenElement = document.querySelector(".first-screen");
 const secondScreenElement = document.querySelector(".second-screen");
 
 const getDeck = () => {
-  const deck = [];
-  figures.forEach((figure) => {
+  suits.forEach((suit) => {
     numbers.forEach((number) => {
-      deck.push({ number: number, figure: figure });
+      deck.push({ number, suit });
     });
   });
-  return deck;
+};
+
+const getRandomCards = () => {
+  const firstCardPosition = Math.floor(Math.random() * deck.length);
+  const secondCardPosition = Math.floor(Math.random() * deck.length);
+
+  const firstCard = deck[firstCardPosition];
+  const secondCard = deck[secondCardPosition];
+
+  console.log(firstCard);
+  console.log(secondCard);
 };
 
 startButtonElement.addEventListener("click", () => {
+  getRandomCards();
   firstScreenElement.classList.toggle("off");
   secondScreenElement.classList.toggle("off");
 });
+
+getDeck();
