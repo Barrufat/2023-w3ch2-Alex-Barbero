29775bc8ae8d4d3235b7bfcce7c9beac3884cae3
Alex Barbero <barrufat@hotmail.com>
2023-09-27 18:10:33 +0200

    Add basic styles to the second screen and inject text values to the DOM elements

--- js/index.js
+++ js/index.js
@@ -1,29 +1,58 @@
-const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+const cardNumbers = [
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+  "A",
+];
 const suits = ["♠", "♣", "♦", "♥"];
-
-const deck = [];
 
 const startButtonElement = document.querySelector(".start-game");
 const firstScreenElement = document.querySelector(".first-screen");
 const secondScreenElement = document.querySelector(".second-screen");
 
 const getDeck = () => {
+  const deck = [];
+
   suits.forEach((suit) => {
-    numbers.forEach((number) => {
-      deck.push({ number, suit });
+    cardNumbers.forEach((cardNumber, numberPosition) => {
+      deck.push({ cardNumber, suit, value: numberPosition });
     });
   });
+
+  return deck;
 };
 
 const getRandomCards = () => {
-  const firstCardPosition = Math.floor(Math.random() * deck.length);
-  const secondCardPosition = Math.floor(Math.random() * deck.length);
+  const userCardPosition = Math.floor(Math.random() * getDeck().length);
+  const gameCardPosition = Math.floor(Math.random() * getDeck().length);
 
-  const firstCard = deck[firstCardPosition];
-  const secondCard = deck[secondCardPosition];
+  const userCard = getDeck()[userCardPosition];
+  const gameCard = getDeck()[gameCardPosition];
 
-  console.log(firstCard);
-  console.log(secondCard);
+  const userCardValues = document.querySelectorAll(".user-card-value");
+  userCardValues[0].textContent = userCard.cardNumber;
+  userCardValues[1].textContent = userCard.cardNumber;
+
+  const userCardSuits = document.querySelectorAll(".user-card-suit");
+  userCardSuits[0].textContent = userCard.suit;
+  userCardSuits[1].textContent = userCard.suit;
+
+  const gameCardValues = document.querySelectorAll(".game-card-value");
+  gameCardValues[0].textContent = gameCard.cardNumber;
+  gameCardValues[1].textContent = gameCard.cardNumber;
+
+  const gameCardSuits = document.querySelectorAll(".game-card-suit");
+  gameCardSuits[0].textContent = gameCard.suit;
+  gameCardSuits[1].textContent = gameCard.suit;
 };
 
 startButtonElement.addEventListener("click", () => {
