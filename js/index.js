cba78d62a74a95223a8c29332cc711afbdc8bd3f
Alex Barbero <barrufat@hotmail.com>
2023-09-27 12:58:39 +0200

    Add basic styles to the screen elements and add visibility-switch functions to the screens

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
 const figures = ["♠", "♣", "♦", "♥"];
+const startButtonElement = document.querySelector(".start-game");
+const firstScreenElement = document.querySelector(".first-screen");
+const secondScreenElement = document.querySelector(".second-screen");
 
 const getDeck = () => {
   const deck = [];
@@ -11,4 +14,7 @@ const getDeck = () => {
   return deck;
 };
 
-console.log(getDeck());
+startButtonElement.addEventListener("click", () => {
+  firstScreenElement.classList.toggle("off");
+  secondScreenElement.classList.toggle("off");
+});
